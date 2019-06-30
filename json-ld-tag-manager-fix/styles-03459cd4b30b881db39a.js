d42587b5130ac4790c74f239c399cc07d4a6b460
Saijo George <ppcinformer@gmail.com>
2019-06-30 12:14:45 +0530

    small fix

--- json-ld-tag-manager-fix/styles-03459cd4b30b881db39a.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{258:function(n,w,o){}}]);
-//# sourceMappingURL=styles-03459cd4b30b881db39a.js.map
--- json-ld-tag-manager-fix/styles-e29d8090d320847cc4c5.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{252:function(n,w,o){}}]);
-//# sourceMappingURL=styles-e29d8090d320847cc4c5.js.map
