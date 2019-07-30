a81943253c163ae8997cb67b48eaadc8a32d36ac
Maciej Leszczyński <dev@asista.pl>
2019-07-30 11:48:50 +0200

    Upload

--- json-ld-schema-generator/sw.js
+++ json-ld-schema-generator/sw.js
@@ -26,40 +26,40 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ff6d9f093d7040a9a0a1.js"
+    "url": "webpack-runtime-616c0f414166f30bae7c.js"
   },
   {
-    "url": "app-fc4bb52c2e6f0df92f0f.js"
+    "url": "app-61c034effba754e76643.js"
   },
   {
-    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-154fa43f003cdd8ba9e6.js"
+    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-6b17097d0d0b54296d10.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "eab647ccc447cf2bb7e9034956bf9235"
+    "revision": "9f59f6ce97864da7383633cd8bfe0b63"
   },
   {
-    "url": "styles.ac631fd3e9859b931460.css"
+    "url": "styles.3edfcea954b5902cb036.css"
   },
   {
-    "url": "component---src-pages-404-js-dcbfff6347e975b80f56.js"
+    "url": "component---src-pages-404-js-266ceb3a63988425fcf9.js"
   },
   {
-    "url": "4-7117f02e423d3581b88e.js"
+    "url": "4-6de4eb70bc74376f6ce5.js"
   },
   {
-    "url": "1-dab61e10c1ddf76212f5.js"
+    "url": "1-1197e3011587cfd043c3.js"
   },
   {
-    "url": "styles-9bbc59f50a9fe50c69e4.js"
+    "url": "styles-f00ffbce7b9d3ca6f3bb.js"
   },
   {
     "url": "page-data/404.html/page-data.json",
-    "revision": "1858f7b3ecda5510e921ac98bf58e66e"
+    "revision": "914701629ac20369ee2c31b07f5fadaa"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "725d312c97e7d9a37dfcae917b58fe4a"
+    "revision": "4bb63641965ee871d37e2a74330eaf99"
   },
   {
     "url": "manifest.webmanifest",
